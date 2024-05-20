09c8e9ac4c671224c25ff4e694c52efab3dabd76
Pedro Henrique Coterli <126521587+PedroPHC25@users.noreply.github.com>
2024-05-20 09:02:39 -0300

    Colocando as explicações do counting sort

--- teste.cpp
+++ teste.cpp
@@ -9,6 +9,50 @@ struct Node {
     Node* ptrPrev;
 };
 
+// Protótipos
+Node* createNode(int);
+void displayList(Node*);
+void insertEnd(Node**, int);
+void removeList(Node**);
+void swapElements(Node*, Node*);
+void displayArray(int[], int);
+void countingSort(Node**);
+
+
+int main() {
+    Node* head = nullptr;
+    
+    // Inserindo elementos na lista
+    insertEnd(&head, 5);
+    insertEnd(&head, 3);
+    insertEnd(&head, 8);
+    insertEnd(&head, 5);
+    insertEnd(&head, 3);
+    insertEnd(&head, 2);
+    insertEnd(&head, 6);
+    insertEnd(&head, 1);
+    insertEnd(&head, 2);
+    insertEnd(&head, 2);
+    insertEnd(&head, 4);
+
+    // Exibindo a lista antes da ordenação
+    cout << "Lista original: ";
+    displayList(head);
+
+    // Ordenando a lista
+    countingSort(&head);
+
+    // Exibindo a lista após a ordenação
+    cout << "Lista ordenada: ";
+    displayList(head);
+
+    // Limpando a lista
+    removeList(&head);
+
+    return 0;
+}
+
+
 // Função para criar um nó
 Node* createNode(int iPayload) {
     Node* temp = (Node*)malloc(sizeof(Node));
@@ -65,7 +109,7 @@ void removeList(Node** head) {
 }
 
 // Função para trocar os payloads de dois nós
-void swapElements(Node* ptrFirst, Node* ptrSecond) {
+void swapElements(Node* ptrFirst, Node* ptrSecond){
     if (ptrFirst == nullptr || ptrSecond == nullptr) {
         cout << "Não é possível trocar pois algum dos ponteiros é nulo." << endl;
         return;
@@ -75,6 +119,18 @@ void swapElements(Node* ptrFirst, Node* ptrSecond) {
     ptrSecond->iPayload = iTemp;
 }
 
+// Função para exibir um array
+void displayArray(int arriValores[], int iTam)
+{
+    cout << "Array: ";
+    for (int i = 0; i < iTam; i++)
+    {
+        cout << arriValores[i] << " ";
+    }
+    cout << endl;
+}
+
+
 void countingSort(Node** head)
 {
     // Checando se o ponteiro que aponta para o head é nulo
@@ -83,6 +139,9 @@ void countingSort(Node** head)
         cout << "Não é possível realizar o counting sort. Ponteiro nulo" << endl;
         return;
     }
+
+    cout << "======================================================= INÍCIO DO ALGORITMO =======================================================" << endl;
+    cout << "-> Primeiro, precisamos encontrar o maior valor da lista:" << endl;
 
     // Setando os ponteiros para fazer a comparação
     Node* current = (*head);
@@ -98,8 +157,17 @@ void countingSort(Node** head)
         iTam ++;
         current = current -> ptrNext;
     }
+    
+    cout << "iMax: " << iMax << endl;
+    cout << "-> Ao mesmo tempo, calculamos o tamanho da lista:" << endl;
+    cout << "iTam: " << iTam << endl;
 
+    cout << "-> Em seguida, criamos um array de zeros do tamanho do maior valor mais 1" << endl;
     int arriCount[iMax+1] = {0};
+    displayArray(arriCount, iMax + 1);
+
+    cout << "-> Após isso, realiza-se a contagem de quantos elementos de cada tipo existem na lista." << endl;
+    cout << "-> Para isso, a cada elemento da lista, somamos 1 à posição do array criado de índice correspondente a esse valor." << endl;
 
     current = (*head);
 
@@ -109,11 +177,17 @@ void countingSort(Node** head)
 
         current = current -> ptrNext;
     }
+    
+    displayArray(arriCount, iMax + 1);
+
+    cout << "-> Agora, fazemos uma soma cumulativa nesse array, de modo que o valor no índice i corresponde a quantos valores menores ou iguais a i existem na lista." << endl;
 
     for (int i = 1; i<=iMax; i++)
     {
         arriCount[i] += arriCount[i-1];
     }
+    
+    displayArray(arriCount, iMax + 1);
     
     current = (*head);
     while (current->ptrNext != nullptr)
@@ -121,7 +195,17 @@ void countingSort(Node** head)
         current = current -> ptrNext;
     }
 
+    cout << "-> Para lista encadeada, criamos nesse passo um novo array de zeros, mas agora do tamanho da lista." << endl;
+    
     int arriSort[iTam] = {0};
+    
+    displayArray(arriSort, iTam);
+
+    cout << "-> A seguir, vem de fato a 'ordenação' da lista." << endl;
+    cout << "-> A ideia é navegar na lista de trás pra frente inserindo cada payload na sua posição correta na lista ordenada." << endl;
+    cout << "-> Essa posição é descoberta olhando para o array de contagem acumulada." << endl;
+    cout << "-> O valor i irá na posição 'arriCount[i] - 1' pois existem 'arriCount[i]' elementos menores ou iguais que i." << endl;
+    cout << "-> Para finalizar esse processo, é decrementado 1 da lista acumulada na posição i para garantir que não haverá sobreposição." << endl;
 
     while (current != nullptr)
     {
@@ -131,6 +215,10 @@ void countingSort(Node** head)
         current = current -> ptrPrev;
     }
 
+    displayArray(arriSort, iTam);
+
+    cout << "-> Por fim, apenas substituímos os valores desse array auxiliar na lista original." << endl;
+    
     current = (*head);
 
     for(int i = 0; i<iTam; i++)
@@ -138,34 +226,8 @@ void countingSort(Node** head)
         current -> iPayload = arriSort[i];
         current = current -> ptrNext;
     }
+    
+    cout << "======================================================= FIM DO ALGORITMO =======================================================" << endl;
 
     return;
 };
-
-int main() {
-    Node* head = nullptr;
-    
-    // Inserindo elementos na lista
-    insertEnd(&head, 5);
-    insertEnd(&head, 3);
-    insertEnd(&head, 8);
-    insertEnd(&head, 1);
-    insertEnd(&head, 7);
-    insertEnd(&head, 2);
-
-    // Exibindo a lista antes da ordenação
-    cout << "Lista original: ";
-    displayList(head);
-
-    // Ordenando a lista
-    countingSort(&head);
-
-    // Exibindo a lista após a ordenação
-    cout << "Lista ordenada: ";
-    displayList(head);
-
-    // Limpando a lista
-    removeList(&head);
-
-    return 0;
-}
